721ec3a128079a5b9667c79490c08191d2469105
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-05-25 21:34:49 +0300

    - created a "city class" it will save the selected city

---
+++ src/main/java/Model/CityName.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class CityName {
+
+    private String cityName;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    @Override
+    public String toString() {
+        return "cityName - " + cityName;
+    }
+}
