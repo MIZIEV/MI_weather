893f8a0f1836ef008dc2adc22ad4bedfd82eafa2
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-05-25 21:39:53 +0300

    - test basic functions - add listener

--- src/main/java/View/FirstWindow.java
+++ src/main/java/View/FirstWindow.java
@@ -1,5 +1,9 @@
 package View;
 
+import Model.CityName;
+import Model.ConnectorToWeatherSite;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,11 +13,18 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.EventListener;
+
 public class FirstWindow {
 
     private final Stage firstWindow = new Stage();
+    CityName cityName;
+    SecondWindow secondWindow;
 
-    public FirstWindow() {
+    public FirstWindow(CityName name, SecondWindow secondW) {
+        this.cityName = name;
+        this.secondWindow = secondW;
+
         String stylesheet = getClass().getResource("/FirstWindow.css").toExternalForm();
 
         BorderPane generalPane = new BorderPane();          //create all panes
@@ -35,15 +46,27 @@ public class FirstWindow {
         Label title = new Label("MI weather program");
         Label textAreaSignature = new Label("Write the name of the city ");
         TextArea inputText = new TextArea();
-        ButtonsPattern startButton = new ButtonsPattern(150,50,"Start");
+        ButtonsPattern startButton = new ButtonsPattern(150, 50, "Start");
         startButton.getStyleClass().add("Button");
 
         inputText.setPrefHeight(50);
         topPane.setCenter(title);
         centralPane.setCenter(centralVBOX);
-                                                      //add all elements in central part
+        //add all elements in central part
         centralVBOX.setAlignment(Pos.CENTER);
-        centralVBOX.getChildren().addAll(textAreaSignature,inputText,startButton);
+        centralVBOX.getChildren().addAll(textAreaSignature, inputText, startButton);
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cityName.setCityName(inputText.getText());
+                System.out.println(cityName.toString());
+
+                firstWindow.close();
+                secondWindow.startWin();
+                ConnectorToWeatherSite connectorToWeatherSite= new ConnectorToWeatherSite();
+            }
+        });
 
         Scene firstWindowScene = new Scene(generalPane, 1024, 768);
         firstWindowScene.getStylesheets().add(stylesheet);
