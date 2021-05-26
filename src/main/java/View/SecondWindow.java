6573c9bd6aa24d469d424030b1f0024d0c586730
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-05-26 23:19:53 +0300

    - add a back to first windows function

--- src/main/java/View/SecondWindow.java
+++ src/main/java/View/SecondWindow.java
@@ -1,6 +1,10 @@
 package View;
 
 import Model.CityName;
+import Model.ConnectorToWeatherSite;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -12,40 +16,55 @@ public class SecondWindow {
 
     Stage secondWindow = new Stage();
     CityName cityName;
+    ConnectorToWeatherSite connectorToWeatherSite;
+    FirstWindow firstWindow;
 
-    public SecondWindow(CityName name) {
+    public SecondWindow(CityName name, ConnectorToWeatherSite connector, FirstWindow firstW) {
         this.cityName = name;
+        this.connectorToWeatherSite = connector;
+        this.firstWindow = firstW;
     }
 
-    public void startWin(){
+    public void startWin() {
         String stylesheet = getClass().getResource("/SecondWindow.css").toExternalForm();
 
         BorderPane generalPane = new BorderPane();
         BorderPane topPane = new BorderPane();
         BorderPane bottomPane = new BorderPane();
         HBox centerHBox = new HBox();
+        centerHBox.setAlignment(Pos.CENTER);
         generalPane.getStyleClass().add("pane");
 
-        String cityN=cityName.getCityName();
+        String cityN = cityName.getCityName();
 
         Label cityName = new Label(cityN);
         topPane.setCenter(cityName);
 
         VBox leftVBox = new VBox();
+        leftVBox.setAlignment(Pos.CENTER);
         Label temperature = new Label("Temperature");
         Label min = new Label("min");
         Label max = new Label("max");
         leftVBox.getChildren().addAll(temperature, min, max);
 
         VBox rightVBox = new VBox();
-        Label tempAnswer = new Label("temp1");
-        Label minAnswer = new Label("min");
-        Label maxAnswer = new Label("max");
+        rightVBox.setAlignment(Pos.CENTER);
+        Label tempAnswer = new Label(connectorToWeatherSite.getTemperature());
+        Label minAnswer = new Label(connectorToWeatherSite.getMinTemp());
+        Label maxAnswer = new Label(connectorToWeatherSite.getMaxTemp());
         rightVBox.getChildren().addAll(tempAnswer, minAnswer, maxAnswer);
 
         ButtonsPattern backButton = new ButtonsPattern(150, 50, "Back");
         backButton.getStyleClass().add("Button");
         bottomPane.setLeft(backButton);
+
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                secondWindow.close();
+                firstWindow.startWin();
+            }
+        });
 
         centerHBox.getChildren().addAll(leftVBox, rightVBox);
         generalPane.setTop(topPane);
