69f8edc2ce7a4cf26283d89090bf441f6792aabb
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-05-26 23:21:13 +0300

    - refactor main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-import Model.CityName;
 import View.FirstWindow;
-import View.SecondWindow;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    CityName cityName = new CityName();
-    SecondWindow secondWindow = new SecondWindow(cityName);
-
     public static void main(String[] args) {
 
         launch(args);
@@ -17,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        FirstWindow firstWindow = new FirstWindow(cityName,secondWindow);
-
+        FirstWindow firstWindow = new FirstWindow();
+        firstWindow.startWin();
     }
 }
