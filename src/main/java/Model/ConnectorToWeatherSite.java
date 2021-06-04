4d3ef5221ada5182ddc5ced05b19707859c7a37f
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-04 21:30:35 +0300

    - add two new methods (getConnectionOnFiveDays & getResponseOnFiveDays) - add 15 new variables and getters & setters for them

--- src/main/java/Model/ConnectorToWeatherSite.java
+++ src/main/java/Model/ConnectorToWeatherSite.java
@@ -1,7 +1,5 @@
 package Model;
 
-import View.ErrorsWindow;
-import javafx.scene.control.Label;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -11,11 +9,29 @@ import java.net.URLConnection;
 
 public class ConnectorToWeatherSite {
 
-    CityName cityName;
-    String temperature;
-    String minTemp;
-    String maxTemp;
-    String clouds;
+    private CityName cityName;
+    private String temperature;
+    private String minTemp;
+    private String maxTemp;
+    private String clouds;
+
+    private String today;
+    private double minTempToday;
+
+
+    private double maxTempToday;
+    private String tomorrowDay;
+    private double minTempTomorrowDay;
+    private double maxTempTomorrowDay;
+    private String dayAfterTomorrow;
+    private double minTempAfterTomorrow;
+    private double maxTempAfterTomorrow;
+    private String fourthDay;
+    private double minTempFourthDay;
+    private double maxTempFourthDay;
+    private String fifthDay;
+    private double minTempFifthDay;
+    private double maxTempFifthDay;
 
     public ConnectorToWeatherSite(CityName name) {
         this.cityName = name;
@@ -33,15 +49,38 @@ public class ConnectorToWeatherSite {
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(openCon.getInputStream()));
             String line;
+
             while ((line = bufferedReader.readLine()) != null) {
                 content.append(line + "\n");
             }
             bufferedReader.close();
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         System.out.println(content);
         this.getResponse(content.toString());
+    }
+
+    public void getConnectionOnFiveDays() {
+        String APICall = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName.getCityName() + "&appid=ceb8e786e2a20dff0a80033639084138";
+        StringBuffer content = new StringBuffer();
+
+        try {
+            URL url = new URL(APICall);
+            URLConnection openCon = url.openConnection();
+            System.out.println(openCon);
+            BufferedReader bufferedReade = new BufferedReader(new InputStreamReader(openCon.getInputStream()));
+            String line;
+
+            while ((line = bufferedReade.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReade.close();
+            System.out.println(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.getResponseOnFiveDays(content.toString());
     }
 
     public void getResponse(String output) {
@@ -50,17 +89,124 @@ public class ConnectorToWeatherSite {
             JSONObject obj = new JSONObject(output);
 
             temperature = obj.getJSONObject("main").getDouble("temp") + " C";
-            minTemp = obj.getJSONObject("main").getDouble("temp_max") + " C ;";
-            maxTemp = obj.getJSONObject("main").getDouble("temp_min") + " C";
+            minTemp = obj.getJSONObject("main").getDouble("temp_min") + " C ;";
+            maxTemp = obj.getJSONObject("main").getDouble("temp_max") + " C";
             clouds = obj.getJSONArray("weather").getJSONObject(0).getString("description");
         }
     }
 
-    public String getTemperature() { return temperature; }
+    public void getResponseOnFiveDays(String output) {
+        if (!output.isEmpty()) {
+            JSONObject obj = new JSONObject(output);
+            double temperature = obj.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp");
 
-    public String getMinTemp() { return minTemp; }
+            today = obj.getJSONArray("list").getJSONObject(0).getString("dt_txt");
+            minTempToday = obj.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp_min");
+            maxTempToday = obj.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp_max");
 
-    public String getMaxTemp() { return maxTemp; }
+            tomorrowDay = obj.getJSONArray("list").getJSONObject(3).getString("dt_txt");
+            minTempTomorrowDay = obj.getJSONArray("list").getJSONObject(3).getJSONObject("main").getDouble("temp_min");
+            maxTempTomorrowDay = obj.getJSONArray("list").getJSONObject(3).getJSONObject("main").getDouble("temp_max");
 
-    public String getClouds() { return clouds; }
+            dayAfterTomorrow = obj.getJSONArray("list").getJSONObject(11).getString("dt_txt");
+            minTempAfterTomorrow = obj.getJSONArray("list").getJSONObject(11).getJSONObject("main").getDouble("temp_min");
+            maxTempTomorrowDay = obj.getJSONArray("list").getJSONObject(11).getJSONObject("main").getDouble("temp_max");
+
+            fourthDay = obj.getJSONArray("list").getJSONObject(19).getString("dt_txt");
+            minTempFourthDay = obj.getJSONArray("list").getJSONObject(19).getJSONObject("main").getDouble("temp_min");
+            maxTempFourthDay = obj.getJSONArray("list").getJSONObject(19).getJSONObject("main").getDouble("temp_max");
+
+            fifthDay = obj.getJSONArray("list").getJSONObject(27).getString("dt_txt");
+            minTempFifthDay = obj.getJSONArray("list").getJSONObject(27).getJSONObject("main").getDouble("temp_min");
+            maxTempFifthDay = obj.getJSONArray("list").getJSONObject(27).getJSONObject("main").getDouble("temp_max");
+
+            System.out.println(today + " -" + minTempToday + " " + maxTempToday);
+            System.out.println(tomorrowDay + " -" + minTempTomorrowDay + " " + maxTempTomorrowDay);
+            System.out.println(dayAfterTomorrow+ " -" + minTempAfterTomorrow + " " + maxTempAfterTomorrow);
+            System.out.println(fourthDay+ " -" + minTempFourthDay + " " + maxTempFourthDay);
+            System.out.println(fifthDay+ " -" + minTempFifthDay + " " + maxTempFifthDay);
+            System.out.println(temperature);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getMinTemp() {
+        return minTemp;
+    }
+
+    public String getMaxTemp() {
+        return maxTemp;
+    }
+
+    public String getClouds() {
+        return clouds;
+    }
+
+    public String getToday() {
+        return today;
+    }
+
+    public String getTomorrowDay() {
+        return tomorrowDay;
+    }
+
+    public String getDayAfterTomorrow() {
+        return dayAfterTomorrow;
+    }
+
+    public String getFourthDay() {
+        return fourthDay;
+    }
+
+    public String getFifthDay() {
+        return fifthDay;
+    }
+
+    public double getMinTempToday() {
+        return minTempToday;
+    }
+
+    public double getMaxTempToday() {
+        return maxTempToday;
+    }
+
+    public double getMinTempTomorrowDay() {
+        return minTempTomorrowDay;
+    }
+
+    public double getMaxTempTomorrowDay() {
+        return maxTempTomorrowDay;
+    }
+
+    public double getMinTempAfterTomorrow() {
+        return minTempAfterTomorrow;
+    }
+
+    public double getMaxTempAfterTomorrow() {
+        return maxTempAfterTomorrow;
+    }
+
+    public double getMinTempFourthDay() {
+        return minTempFourthDay;
+    }
+
+    public double getMaxTempFourthDay() {
+        return maxTempFourthDay;
+    }
+
+    public double getMinTempFifthDay() {
+        return minTempFifthDay;
+    }
+
+    public double getMaxTempFifthDay() {
+        return maxTempFifthDay;
+    }
 }
