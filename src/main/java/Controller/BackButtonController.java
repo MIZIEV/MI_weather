898eb256d2eab6b3d528bde3058b22b9812198ae
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-11 23:18:45 +0300

    - added general class description

--- src/main/java/Controller/BackButtonController.java
+++ src/main/java/Controller/BackButtonController.java
@@ -1,4 +1,8 @@
 package Controller;
+/**
+ * this controller class set NULL to the two variables
+ * this is need too call error window if client come back to start window and will not set any city name
+ */
 
 import Model.JSONDataParser;
 
@@ -10,8 +14,8 @@ public class BackButtonController {
         this.parser = jsonParser;
     }
 
-    public void deletingTwoValues(){
-        parser.setTemperature(null);
-        parser.setToday(null);
+    public void deletingTwoValues() {
+        parser.setTempNow(null);
+        parser.setTodayDate(null);
     }
 }
