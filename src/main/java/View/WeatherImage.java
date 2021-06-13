a7602a168736ee2fb1e537fa77494dcc49242405
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-13 22:40:07 +0300

    - added new method for selecting weather picture

--- src/main/java/View/WeatherImage.java
+++ src/main/java/View/WeatherImage.java
@@ -2,36 +2,45 @@ package View;
 
 import Model.JSONDataParser;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 
 public class WeatherImage {
+
     private final JSONDataParser parser;
     private ImageView weatherImage;
 
-    private final static String DAY_CLEAR_SKY = "/WeatherIcons/_clear_sky.jpg";
-    private final static String NIGHT_CLEAR_SKY = "/WeatherIcons/_clear_sky.jpg";
-    private final static String DAY_LIGHT_RAIN = "/WeatherIcons/_light_rain.jpg";
-    private final static String NIGHT_LIGHT_RAIN = "/WeatherIcons/_light_rain.jpg";
-    private final static String DAY_OVERCAST_CLOUDS = "/WeatherIcons/_overcast_clouds.jpg";
-    private final static String NIGHT_OVERCAST_CLOUDS = "/WeatherIcons/_overcast_clouds.jpg";
+    private final static String DAY_CLEAR_SKY = "/WeatherIcons/day_clear_sky.jpg";
+    private final static String DAY_LIGHT_RAIN = "/WeatherIcons/day_light_rain.jpg";
+    private final static String DAY_OVERCAST_CLOUDS = "/WeatherIcons/day_overcast_clouds.jpg";
 
     public WeatherImage(JSONDataParser jsonDataParser) {
         this.parser = jsonDataParser;
     }
 
-    public void selectPicture(BorderPane pane, String nowTime) {
+    public ImageView selectTodayPicture(String nowTime) {
         if (parser.getWeatherNow().equals("scattered clouds") ^ parser.getWeatherNow().equals("few clouds") ^
                 parser.getWeatherNow().equals("broken clouds") ^ parser.getWeatherNow().equals("overcast clouds")) {
             weatherImage = new ImageView("/WeatherIcons/" + nowTime + "_overcast_clouds.jpg");
-            pane.setCenter(weatherImage);
         }
         if (parser.getWeatherNow().equals("light rain") ^ parser.getWeatherNow().equals("moderate rain")) {
             weatherImage = new ImageView("/WeatherIcons/" + nowTime + "_light_rain.jpg");
-            pane.setCenter(weatherImage);
         }
         if (parser.getWeatherNow().equals("clear sky")) {
             weatherImage = new ImageView("/WeatherIcons/" + nowTime + "_clear_sky.jpg");
-            pane.setCenter(weatherImage);
         }
+        return weatherImage;
+    }
+
+    public ImageView selectPicture(String weather) {
+        if (weather.equals("scattered clouds") ^ weather.equals("few clouds") ^ weather.equals("broken clouds")
+                ^ weather.equals("overcast clouds")) {
+            weatherImage = new ImageView(DAY_OVERCAST_CLOUDS);
+        }
+        if (weather.equals("light rain") ^ weather.equals("moderate rain")) {
+            weatherImage = new ImageView(DAY_LIGHT_RAIN);
+        }
+        if (weather.equals("clear sky")) {
+            weatherImage = new ImageView(DAY_CLEAR_SKY);
+        }
+        return weatherImage;
     }
 }
