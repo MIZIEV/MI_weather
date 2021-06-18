0c9e1dc5342ccce7ebbd7f0f18d749c590ea6b59
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-18 23:35:32 +0300

    - added new constructor - added new method, it will return min & max temperature in label, and not in Second Window class thereby optimize their class

--- src/main/java/View/LabelPattern.java
+++ src/main/java/View/LabelPattern.java
@@ -1,11 +1,43 @@
 package View;
 
+import Model.DataAnalyser;
+import Model.IndexClass;
+import Model.JSONDataParser;
 import javafx.scene.control.Label;
 
 public class LabelPattern extends Label {
 
+    private JSONDataParser parser;
+    private DataAnalyser analyser;
+    private String labelText;
+    private IndexClass indexClass;
+
     public LabelPattern(String text, String styleClass) {
         super(text);
         getStyleClass().add(styleClass);
+    }
+
+    public LabelPattern(IndexClass indexClass, String text, JSONDataParser parser, DataAnalyser analyser, String styleClass) {
+        this.parser = parser;
+        this.analyser = analyser;
+        this.labelText = text;
+        this.indexClass=indexClass;
+        setText(text + this.getTemp() + "°C");
+        getStyleClass().add(styleClass);
+    }
+
+    private int getTemp() {
+        int temp = 0;
+        if (labelText.equals("min ")) {
+            temp = analyser.minTemp(parser.getTempMap().
+                    tailMap(parser.getKeysForMap().get(indexClass.getStartDayIndex())).
+                    headMap(parser.getKeysForMap().get(indexClass.getEndDayIndex() + 1))) - 273;
+            return temp;
+        } else if (labelText.equals("max ")) {
+            temp = analyser.maxTemp(parser.getTempMap().
+                    tailMap(parser.getKeysForMap().get(indexClass.getStartDayIndex())).
+                    headMap(parser.getKeysForMap().get(indexClass.getEndDayIndex() + 1))) - 273;
+            return temp;
+        } else return temp;
     }
 }
