c386649432a998d9987202c79fc3f87bd5e51155
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-20 21:14:16 +0300

    - added default method

--- src/main/java/Controller/ControllersForDiagram/InfoButtonsControllers.java
+++ src/main/java/Controller/ControllersForDiagram/InfoButtonsControllers.java
@@ -1,5 +1,11 @@
 package Controller.ControllersForDiagram;
 
+import View.Diagram;
+
 public interface InfoButtonsControllers {
     void putDataToDiagram();
+
+    default void clearDataFromDiagram(Diagram diagram){
+        diagram.getTemp().getData().clear();
+    }
 }
