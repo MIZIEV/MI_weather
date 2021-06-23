733d613a04fd46d7b1589f73a099ebae23d19a0d
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-23 22:54:59 +0300

    - global changed first window design

--- src/main/java/View/FirstWindow.java
+++ src/main/java/View/FirstWindow.java
@@ -4,74 +4,72 @@ import Controller.StartButtonController;
 import Model.CityName;
 import Model.ConnectorToWeatherSite;
 import Model.JSONDataParser;
+import View.Patterns.ButtonsPattern;
 import animatefx.animation.FadeIn;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FirstWindow {
 
     private final Stage firstWindow = new Stage();
-    private final TextArea inputText = new TextArea();
+    private final TextField inputText = new TextField();
 
     private final CityName cityName = new CityName();
     private final ConnectorToWeatherSite connector = new ConnectorToWeatherSite(cityName);
 
     private final static String FIRST_WINDOW_TITLE = "Mi weather program";
     private final static String STYLE_CLASS_BUTTON = "Button";
-    private final ImageView image = new ImageView("white_picture.jpg");
-    private final static short IMAGE_WIDTH = 600;
-    private final static short IMAGE_HEIGHT = 200;
-    private final static short WINDOW_WIDTH = 1024;
+    private final ImageView image = new ImageView("title_icon.jpg");
+
+    private final static short IMAGE_WIDTH = 300;
+    private final static short IMAGE_HEIGHT = 300;
+
+    private final static short WINDOW_WIDTH = 450;
     private final static short WINDOW_HEIGHT = 768;
-    private final static short WINDOW_MIN_WIDTH = 880;
-    private final static short WINDOW_MIN_HEIGHT = 650;
+    private final static short WINDOW_MIN_WIDTH = 400;
+    private final static short WINDOW_MIN_HEIGHT = 700;
 
     public void startWin() {
         Insets margin = new Insets(40);
 
-        JSONDataParser parser = new JSONDataParser();
+        JSONDataParser parser = new JSONDataParser(cityName);
         SecondWindow secondWindow = new SecondWindow(cityName, parser, this);
         StartButtonController startButtonController = new StartButtonController(connector, parser, this, cityName);
-        String stylesheet = getClass().getResource("/FirstWindow.css").toExternalForm();
+        String stylesheet = getClass().getResource("/VisualStyles/FirstWindow.css").toExternalForm();
 
-        BorderPane generalPane = new BorderPane();          //create all panes
+        BorderPane mainPane = new BorderPane();          //create all panes
         BorderPane centralPane = new BorderPane();
-        VBox topVBox = new VBox(30);
-        Pane leftInsert = new Pane();
-        Pane rightInsert = new Pane();
-        Pane bottomInsert = new Pane();
+        HBox topHBox = new HBox(30);
+        BorderPane.setMargin(topHBox,margin);
+        topHBox.getStyleClass().add("HBox");
 
-        leftInsert.setPrefWidth(200);
-        rightInsert.setPrefWidth(200);
-        bottomInsert.setPrefHeight(150);
-        generalPane.getStyleClass().add("pane");
+        mainPane.getStyleClass().add("pane");
 
-        generalPane.setTop(topVBox);
-        generalPane.setCenter(centralPane);
-        generalPane.setLeft(leftInsert);
-        generalPane.setRight(rightInsert);
-        generalPane.setBottom(bottomInsert);
+        mainPane.setTop(topHBox);
+        mainPane.setCenter(centralPane);
 
         VBox centralVBOX = new VBox(30);                      // create central VBOX and add all elements
-        Label title = new Label("MI weather program");
         Label textAreaSignature = new Label("Write the city name");
         ButtonsPattern startButton = new ButtonsPattern(150, 50, "Start", STYLE_CLASS_BUTTON);
 
-        inputText.setPrefHeight(50);
+        inputText.getStyleClass().add("text-input");
+        inputText.setMaxSize(350,50);
+
+        inputText.positionCaret(5);
         image.setFitWidth(IMAGE_WIDTH);
         image.setFitHeight(IMAGE_HEIGHT);
-        topVBox.setAlignment(Pos.CENTER);
-        BorderPane.setMargin(topVBox, margin);
-        topVBox.getChildren().addAll(title, image);
+        topHBox.setAlignment(Pos.CENTER);
+        BorderPane.setMargin(topHBox, margin);
+        topHBox.getChildren().addAll( image);
         centralPane.setCenter(centralVBOX);
 
         centralVBOX.setAlignment(Pos.CENTER);                           //add all elements in central part
@@ -96,18 +94,18 @@ public class FirstWindow {
             }
         });
 
-        Scene firstWindowScene = new Scene(generalPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        Scene firstWindowScene = new Scene(mainPane, WINDOW_WIDTH, WINDOW_HEIGHT);
         firstWindowScene.getStylesheets().add(stylesheet);
         firstWindow.getIcons().add(new Image("weather_icon.png"));
         firstWindow.setMinWidth(WINDOW_MIN_WIDTH);
         firstWindow.setMinHeight(WINDOW_MIN_HEIGHT);
         firstWindow.setScene(firstWindowScene);
         firstWindow.setTitle(FIRST_WINDOW_TITLE);
-        new FadeIn(generalPane).play();
+        new FadeIn(mainPane).play();
         firstWindow.show();
     }
 
-    public TextArea getInputText() {
+    public TextField getInputText() {
         return inputText;
     }
 }
