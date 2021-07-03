714edbcff9a1328195e351c7e1b8d93b1f9f91c9
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-07-03 19:07:31 +0300

    - added class description - added icon size constants - renamed methods - refactor code

--- src/main/java/View/WeatherImage.java
+++ src/main/java/View/WeatherImage.java
@@ -3,52 +3,55 @@ package View;
 import Model.JSONDataParser;
 import javafx.scene.image.ImageView;
 
+/**
+ * class description:
+ * This class is needed to choosing tha weather image.
+ * getTodayImage method takes strings with present time & theme, then depending on the weather, it return images.
+ * selectImage method takes strings with weather & theme, then return weather images without time dependency
+ */
 public class WeatherImage {
 
     private final JSONDataParser parser;
     private ImageView weatherImage;
-
-    private final static String DAY_CLEAR_SKY = "/VisualStyles/DarkTheme/DarkWeatherIcons/day_clear_sky.jpg";
-    private final static String DAY_LIGHT_RAIN = "/VisualStyles/DarkTheme/DarkWeatherIcons/day_light_rain.jpg";
-    private final static String DAY_OVERCAST_CLOUDS = "/VisualStyles/DarkTheme/DarkWeatherIcons/day_overcast_clouds.jpg";
+    private final static short TODAY_IMAGE_SIZE = 145;
+    private final static byte IMAGE_SIZE = 100;
 
     public WeatherImage(JSONDataParser jsonDataParser) {
         this.parser = jsonDataParser;
     }
 
-    public ImageView selectTodayPicture(String nowTime, String stylesheet) {
-        String theme;
-        if (stylesheet.equals("/VisualStyles/DarkTheme/")) theme = "DarkTheme/DarkWeatherIcons/";
-        else theme = "LightTheme/LightWeatherIcons/";
+    public ImageView getTodayImage(String nowTime, String styleTheme) {
 
         if (parser.getWeatherNow().equals("scattered clouds") ^ parser.getWeatherNow().equals("few clouds") ^
                 parser.getWeatherNow().equals("broken clouds") ^ parser.getWeatherNow().equals("overcast clouds")) {
-            weatherImage = new ImageView("/VisualStyles/" + theme + nowTime + "_overcast_clouds.jpg");
+            weatherImage = new ImageView(styleTheme +"WeatherIcons/"+ nowTime + "_overcast_clouds.jpg");
         }
-        if (parser.getWeatherNow().equals("light rain") ^ parser.getWeatherNow().equals("moderate rain")) {
-            weatherImage = new ImageView("/VisualStyles/" + theme + nowTime + "_light_rain.jpg");
+        if (parser.getWeatherNow().equals("light rain") ^ parser.getWeatherNow().equals("moderate rain")
+                ^ parser.getWeatherNow().equals("heavy intensity rain")) {
+            weatherImage = new ImageView(styleTheme +"WeatherIcons/"+ nowTime + "_light_rain.jpg");
         }
         if (parser.getWeatherNow().equals("clear sky")) {
-            weatherImage = new ImageView("/VisualStyles/" + theme + nowTime + "_clear_sky.jpg");
+            weatherImage = new ImageView(styleTheme+"WeatherIcons/" + nowTime + "_clear_sky.jpg");
         }
-        weatherImage.setFitWidth(145);
-        weatherImage.setFitHeight(145);
+        weatherImage.setFitWidth(TODAY_IMAGE_SIZE);
+        weatherImage.setFitHeight(TODAY_IMAGE_SIZE);
         return weatherImage;
     }
 
-    public ImageView selectPicture(String weather) {
+    public ImageView selectImage(String weather, String styleTheme) {
+
         if (weather.equals("scattered clouds") ^ weather.equals("few clouds") ^ weather.equals("broken clouds")
                 ^ weather.equals("overcast clouds")) {
-            weatherImage = new ImageView(DAY_OVERCAST_CLOUDS);
+            weatherImage = new ImageView(styleTheme + "WeatherIcons/day_overcast_clouds.jpg");
         }
-        if (weather.equals("light rain") ^ weather.equals("moderate rain")) {
-            weatherImage = new ImageView(DAY_LIGHT_RAIN);
+        if (weather.equals("light rain") ^ weather.equals("moderate rain") ^ weather.equals("heavy intensity rain")) {
+            weatherImage = new ImageView(styleTheme + "WeatherIcons/day_light_rain.jpg");
         }
         if (weather.equals("clear sky")) {
-            weatherImage = new ImageView(DAY_CLEAR_SKY);
+            weatherImage = new ImageView( styleTheme + "WeatherIcons/day_clear_sky.jpg");
         }
-        weatherImage.setFitWidth(100);
-        weatherImage.setFitHeight(100);
+        weatherImage.setFitWidth(IMAGE_SIZE);
+        weatherImage.setFitHeight(IMAGE_SIZE);
         return weatherImage;
     }
 }
