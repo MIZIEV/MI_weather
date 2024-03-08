09c02493c34b22cb433840a9b16c17a8a0dfd486
Olexandr Miziev <mizzzev@outlook.com>
2024-03-08 17:48:05 +0200

    - added entities (I don't remember why it untracked)

---
+++ src/main/java/prog/models/DataAnalyser.java
@@ -0,0 +1,31 @@
+package prog.models;
+
+import java.util.Iterator;
+import java.util.SortedMap;
+
+public class DataAnalyser {
+
+    public int maxTemp(SortedMap<String, Integer> map) {
+        Iterator<Integer> iterator = map.values().iterator();
+        int max = map.get(map.firstKey());
+        while (iterator.hasNext()) {
+            int element = iterator.next();
+            if (max < element) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    public int minTemp(SortedMap<String, Integer> map) {
+        Iterator<Integer> iterator = map.values().iterator();
+        int min = map.get(map.firstKey());
+        while (iterator.hasNext()) {
+            int element = iterator.next();
+            if (min > element) {
+                min = element;
+            }
+        }
+        return min;
+    }
+}
